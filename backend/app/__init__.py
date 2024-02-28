1a29000844652eba70b9b23cebdc24792a066eb3
Lucas <junsuny789@gmail.com>
2024-02-27 23:50:52 -0500

    mongodb -> firebase

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,5 +3,5 @@ from .services.db import init_db
 
 def create_app():
     app = Flask(__name__)
-    init_db(app)  # Initialize MongoDB connection
+    init_db(app)  # Initialize Firebase connection
     return app
