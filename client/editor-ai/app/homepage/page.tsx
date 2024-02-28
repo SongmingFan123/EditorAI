1649f251398c7880be24864da896fb639c186905
Lucas <junsuny789@gmail.com>
2024-02-28 00:20:39 -0500

    nextjs init

---
+++ client/editor-ai/app/homepage/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const homepage = () => {
+  const priorityProjects = [{ name: 'Project 1' }, { name: 'Project 2' }];
+  const recentProjects = [{ name: 'Project 3' }, { name: 'Project 4' }];
+
+  return (
+    
+    <div className = "homepage"> 
+        this is homepage
+    </div>
+    
+  );
+};
+
+export default homepage;
---
+++ client/editor-ai/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+
+function App() {
+  return (
+    <main>
+      <Link href={"/homepage"}>Go to Homepage</Link>
+    </main>
+
+  );
+}
+
+export default App;
