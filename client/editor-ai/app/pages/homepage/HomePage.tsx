1e330849f4544b33a98079dd85227beec81d5e89
Ben Gardiner <bengardiner18@gmail.com>
2024-04-11 01:15:57 -0400

    created doc fail error message

--- client/editor-ai/app/pages/homepage/HomePage.tsx
+++ client/editor-ai/app/pages/homepage/HomePage.tsx
@@ -22,15 +22,15 @@ const HomePage = () => {
 
   const handlePopupSubmit = async () => {
     const documentId = await handleCreateDocument(userId, documentName, "");
-
-    if (documentId != null) {
+    console.log('DocumentId:', documentId);
+    if (documentId ==true) {
       setCreateDocumentFailed(false);
-      router.push(`./texteditor/?documentid=${documentId}`); // Pass documentId instead of key
+      setShowPopup(false);
+
     } else {
       setCreateDocumentFailed(true);
     }
 
-    setShowPopup(false);
   };
 
   return (
@@ -44,27 +44,30 @@ const HomePage = () => {
         <div className="bg-slate-200">
           <div className="popup-content flex flex-col">
             {createDocumentFailed && <h1>That document name already exists. Select a new name.</h1>}
-            <input
-              type="text"
-              placeholder="Enter document name"
-              value={documentName}
-              onChange={(e) => setDocumentName(e.target.value)}
-              className="rounded-lg p-2 m-2"
-            />
-            <div className='flex flex-row'>
-              <button
-                onClick={handlePopupSubmit}
-                className="bg-brand-red text-white rounded-full p-2 m-2"
-              >
-                Submit
-              </button>
-              <button
-                onClick={handlePopupClose}
-                className="bg-brand-red text-white rounded-full p-2 m-2"
-              >
-                Cancel
-              </button>
-            </div>
+            <>
+              <input
+                type="text"
+                placeholder="Enter document name"
+                value={documentName}
+                onChange={(e) => setDocumentName(e.target.value)}
+                className="rounded-lg p-2 m-2"
+              />
+              <div className='flex flex-row'>
+                <button
+                  onClick={handlePopupSubmit}
+                  className="bg-brand-red text-white rounded-full p-2 m-2"
+                >
+                  Submit
+                </button>
+                <button
+                  onClick={handlePopupClose}
+                  className="bg-brand-red text-white rounded-full p-2 m-2"
+                >
+                  Cancel
+                </button>
+              </div>
+            </>
+            
           </div>
         </div>
       )}
