57e5002ea8b7a5c9fe275af00d4c7076778a8f58
Ben Gardiner <bengardiner18@gmail.com>
2024-04-19 15:47:28 -0400

    merging with dev

--- client/editor-ai/app/pages/homepage/HomePage.tsx
+++ client/editor-ai/app/pages/homepage/HomePage.tsx
@@ -7,6 +7,7 @@ import ActionButton from './ActionButton';
 import { useRouter } from 'next/navigation';
 import { handleCreateDocument } from '../../api/document_functions';
 import UploadButton from './UploadButton';
+import DocumentCreation from './DocumentCreation';
 
 const HomePage = () => {
   const [documentName, setDocumentName] = useState('');
@@ -46,40 +47,16 @@ const HomePage = () => {
       <SearchBar onSearchQueryChange={handleSearchQueryChange} />
       <div className="flex">
         <ActionButton text="Create Document" onClick={() => setShowPopup(true)} />
-        {/* <UploadButton setContent={setDocumentContent} setSelectedFile={setDocumentName} /> */}
         <UploadButton createDocument={handleCreateDocument} />
 
       </div>
       {showPopup && (
-        <div className="bg-slate-200">
-          <div className="popup-content flex flex-col">
-            {/* {createDocumentFailed && <h1>That document name already exists. Select a new name.</h1>} */}
-            <>
-              <input
-                type="text"
-                placeholder="Enter document name"
-                value={documentName}
-                onChange={(e) => setDocumentName(e.target.value)}
-                className="rounded-lg p-2 m-2"
-              />
-              <div className='flex flex-row'>
-                <button
-                  onClick={handlePopupSubmit}
-                  className="bg-brand-red text-white rounded-full p-2 m-2"
-                >
-                  Submit
-                </button>
-                <button
-                  onClick={handlePopupClose}
-                  className="bg-brand-red text-white rounded-full p-2 m-2"
-                >
-                  Cancel
-                </button>
-              </div>
-            </>
-            
-          </div>
-        </div>
+        <DocumentCreation
+          documentName={documentName}
+          setDocumentName={setDocumentName}
+          handlePopupSubmit={handlePopupSubmit}
+          handlePopupClose={handlePopupClose}
+        />
       )}
       <div className= 'mb4' style={{ height: '1.5px', background: 'rgba(128, 18, 18, 1)', width: '100%', position: 'relative', top: '-10px', font: 'Bold'}}></div>
       <div className="flex font-newsreader">
