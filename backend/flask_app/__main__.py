7bef0eae37e3a38b15721d6fc2727db33abb84af
Lucas <junsuny789@gmail.com>
2024-04-25 23:40:15 -0400

    newnew1234

--- backend/flask_app/__main__.py
+++ backend/flask_app/__main__.py
@@ -2,9 +2,9 @@ from flask_app import create_app
 
 # Every backend operation starts here!
 
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
 
     # app.run(
     #     debug=False,
