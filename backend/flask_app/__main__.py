a416df6d36e9929927e5508b12e87fcf399b6878
Lucas <junsuny789@gmail.com>
2024-04-25 23:07:14 -0400

    asdf1

--- backend/flask_app/__main__.py
+++ backend/flask_app/__main__.py
@@ -1,10 +1,10 @@
-from app import create_app
+from flask_app import create_app
 
 # Every backend operation starts here!
 
-app = create_app()
 
 if __name__ == '__main__':
+    app = create_app()
 
     # app.run(
     #     debug=False,
