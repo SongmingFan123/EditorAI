997e8538bbe7c5d8c50629f5c89a23509c25efd6
Lucas <junsuny789@gmail.com>
2024-04-25 23:50:27 -0400

    asdfasdfasdfasf

--- backend/flask_app/__main__.py
+++ backend/flask_app/__main__.py
@@ -4,11 +4,11 @@ from flask_app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
     # app.run(
     #     debug=False,
     #     threaded=True
     # )
 
-    app.run(host='0.0.0.0', debug=False)
+app.run(host='0.0.0.0', debug=False)
