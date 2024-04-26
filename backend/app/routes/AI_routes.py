b8591e3c66d7e72e977d6d895225ffb95662bca1
Lucas <junsuny789@gmail.com>
2024-04-25 21:35:06 -0400

    new1

--- backend/app/routes/AI_routes.py
+++ backend/app/routes/AI_routes.py
@@ -10,42 +10,45 @@ CORS(bp)
 @cross_origin()
 def suggest_title():
     """ """
-    try:
-        AIconfig = editorai_chatbot()
-        data = request.json
-        content = AIconfig.create_title(data["document"])
-        if(not content):
-            return requestHandler.handle_server_error
-        return handle_success(content)
-    except Exception as e:
-         handle_server_error(e)
+    pass
+    # try:
+    #     AIconfig = editorai_chatbot()
+    #     data = request.json
+    #     content = AIconfig.create_title(data["document"])
+    #     if(not content):
+    #         return requestHandler.handle_server_error
+    #     return handle_success(content)
+    # except Exception as e:
+    #      handle_server_error(e)
 
 @bp.route('/summarize', methods=['POST'])
 @cross_origin
 def summarize_article():
-    try:
-        AIconfig = editorai_chatbot()
+    pass
+    # try:
+    #     AIconfig = editorai_chatbot()
 
-        data = request.json
-        content = AIconfig.summarize_article(data["document"])
-        if(not content):
-            return requestHandler.handle_server_error
-        return handle_success(content)
-    except Exception as e:
-        handle_server_error(e)
+    #     data = request.json
+    #     content = AIconfig.summarize_article(data["document"])
+    #     if(not content):
+    #         return requestHandler.handle_server_error
+    #     return handle_success(content)
+    # except Exception as e:
+    #     handle_server_error(e)
 
 @bp.route('/sns', methods=['POST'])
 @cross_origin
 def make_sns():
-    try:
-        AIconfig = editorai_chatbot()
+    pass
+    # try:
+    #     AIconfig = editorai_chatbot()
 
-        data = request.json
-        content = AIconfig.create_social_media_copy(data["document"])
-        if(not content):
-            return requestHandler.handle_server_error
-        return handle_success(content)
-    except Exception as e:
-        handle_server_error(e)
+    #     data = request.json
+    #     content = AIconfig.create_social_media_copy(data["document"])
+    #     if(not content):
+    #         return requestHandler.handle_server_error
+    #     return handle_success(content)
+    # except Exception as e:
+    #     handle_server_error(e)
 
-    return content
+    # return content
