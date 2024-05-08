5af8fce8514b2d1bb1e167161fd7eb8167a00638
Lucas Yoon <94267691+JslYoon@users.noreply.github.com>
2024-05-07 22:16:10 -0400

    Revert "Final touches"

--- backend/flask_app/services/__init__.py
+++
@@ -1,3 +0,0 @@
-from .firestore_service import firestore_service
-
-# Takes care of all the backend services (external API, firestore database, authentication)
--- backend/flask_app/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .requestHandler import *
-from .dateHandler import *
-
-# Utility functions/classes that act as helper functions
